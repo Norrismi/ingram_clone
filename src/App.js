4ddbb1b6c3a8fd8d2de419a6103acefea0296d8a
Norrismi <Norrismi0@gmail.com>
2022-05-04 16:01:37 -0400

    working on header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import Header from './Components/Header/Header'
 
+
 function App() {
   return (
     <div className="App">
   
 
       <Header/>
+   
 
- 
+
+
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,25 +1,26 @@
 import React from 'react'
+import { Button } from 'antd';
 import './Header.css'
+import { Typography } from 'antd';
+
+const { Text } = Typography;
 
 function Header() {
     return (
         <div className='header-container'>
             <div className="header-btns">
-                <div id='btn'>Shop</div>
-                <div id='btn'>Cloud</div>
-                <div id='btn'>Commerce \& Lifecycle</div>
-                <div id='btn'>IoT</div>
+                <Button type="link" block id='btn'>Shop</Button>
+                <Button type="link" block id='btn'>Cloud</Button>
+                <Button type="link" block id='btn'>Commerce & Lifecycle</Button>
+                <Button type="link" block id='btn'>IoT</Button>
             </div>
 
-
-            <div className='header-location'>
-                <div id='btn'>
-                    Country:
-                </div>
-                <div id='btn'>
-                    United States
-                </div>
-            </div>
+            {/* <div className='header-location'>
+                <Button type="link" block id='btn'>County:</Button>
+                <Button type="link" block id='btn'>
+                    <Text underline>United States</Text>
+                </Button>
+            </div> */}
         </div>
     )
 }
