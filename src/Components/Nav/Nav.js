8a3070b36dba50a9c1f4c328e033b8022c7829cf
Norrismi <Norrismi0@gmail.com>
2022-05-05 16:16:41 -0400

    fixed first nav

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Button } from 'antd';
 import './Nav.css'
 import { Typography } from 'antd';
+import { Row, Col } from 'antd';
 
 const { Text } = Typography;
 
@@ -9,22 +10,29 @@ function Header() {
     return (
         <div className='header-container'>
             <div className="header-navbar">
-
                 <div className="header-btns">
-                    <Button type="link" block id='btn'>Shop</Button>
-                    <Button type="link" block id='btn'>Cloud</Button>
-                    <Button type="link" block id='btn'>Commerce & Lifecycle</Button>
-                    <Button type="link" block id='btn'>IoT</Button>
-                </div>
-
-                <div className='header-location'>
-                    <Button type="link" block id='btn'>County:</Button>
-                    <Button type="link" block id='btn'>
-                        <Text underline>United States</Text>
-                    </Button>
+                    <Row>
+                        <Col flex="100px" className="nav__logo">
+                            <Button type="link" block id='btn'>Shop</Button>
+                        </Col>
+                        <Col flex="100px">
+                            <Button type="link" block id='btn'>Cloud</Button>
+                        </Col>
+                        <Col flex="100px">
+                            <Button type="link" block id='btn'>Commerce & Lifecycle</Button>
+                        </Col>
+                        <Col flex="100px" >
+                            <Button type="link" block id='btn'>IoT</Button>
+                        </Col>
+                        <Col span={4} push={10} >
+                            <Button type="link" block id='btn'>County:</Button>
+                        </Col>
+                        <Col span={4} push={8}>
+                            <Button type="link" block id='btn'><Text underline>United States</Text></Button>
+                        </Col>
+                    </Row>
                 </div>
             </div>
-
         </div>
     )
 }
--- src/Components/NavSecondary/NavSecondary.js
+++ src/Components/NavSecondary/NavSecondary.js
@@ -17,25 +17,19 @@ function NavSecondary() {
     return (
         <div className='nav__container'>
             <div className="nav__search">
-
                 <Row align="middle">
                     <Col flex="185px" className="nav__logo">
                         <img src={ingramPic} className="nav__logo-ingram" alt="Logo" />
                     </Col>
 
                     <Col flex="auto">
-                        <Search className="nav__searchbar" placeholder="input search text" onSearch={onSearch} style={{ width: 650 }} />
+                        <Search className="nav__searchbar" placeholder="input search text" onSearch={onSearch} />
                     </Col>
 
                     <Col flex="200px" justify="end">
                         <Button className="nav__btn" ghost> LOG IN</Button>
                     </Col>
                 </Row>
-
-
-        
-
-
             </div>
 
             <div className="nav__menu">
